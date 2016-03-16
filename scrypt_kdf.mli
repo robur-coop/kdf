59d60be92e5ec8b02abd61f0adaf4a3970f9820d
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2016-03-16 23:08:46 +0100

    Rename scrypt function to scrypt_kdf

--- scrypt_kdf.mli
+++ scrypt_kdf.mli
@@ -1,1 +1,1 @@
-val scrypt : password:Cstruct.t -> salt:Cstruct.t -> n:int -> r:int -> p:int -> dk_len:int32 -> Cstruct.t
+val scrypt_kdf : password:Cstruct.t -> salt:Cstruct.t -> n:int -> r:int -> p:int -> dk_len:int32 -> Cstruct.t
