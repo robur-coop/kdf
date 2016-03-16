de0d538bc4cabcb7ec6b778ac41c96af00ddf2b8
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2016-03-16 13:54:15 +0100

    Implement scryptBlockMix algorithm

---
+++ scrypt_kdf.ml
@@ -0,0 +1,13 @@
+open Nocrypto.Uncommon
+
+let scrypt_block_mix i r =
+  let o = Cstruct.create (Cstruct.len i) in
+  let x = ref (Cstruct.sub i ((2 * r - 1) * 64) 64) in
+  for j = 0 to 2 * r - 1 do
+    let b_i = Cstruct.sub i (j * 64) 64 in
+    Cs.xor_into !x b_i 64;
+    x := Salsa20_core.salsa20_8_core b_i;
+    let offset = ((j mod 2) lsl (r - 1)) + j / 2 in
+    Cstruct.blit !x 0 o (offset * 64) 64
+  done;
+  o;;
