c94e295a4af78577226670adceb34c2fd4c384cc
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2016-03-16 15:00:56 +0100

    Rename some variables to make them more consistent with the spec

--- scrypt_kdf.ml
+++ scrypt_kdf.ml
@@ -1,11 +1,11 @@
-let scrypt_block_mix i r =
-  let o = Cstruct.create (Cstruct.len i)
-  and x = ref (Cstruct.sub i ((2 * r - 1) * 64) 64) in
-  for j = 0 to 2 * r - 1 do
-    let b_i = Cstruct.sub i (j * 64) 64 in
+let scrypt_block_mix b r =
+  let b' = Cstruct.create (Cstruct.len b)
+  and x = ref (Cstruct.sub b ((2 * r - 1) * 64) 64) in
+  for i = 0 to 2 * r - 1 do
+    let b_i = Cstruct.sub b (i * 64) 64 in
     Nocrypto.Uncommon.Cs.xor_into !x b_i 64;
     x := Salsa20_core.salsa20_8_core b_i;
-    let offset = ((j mod 2) lsl (r - 1)) + j / 2 in
-    Cstruct.blit !x 0 o (offset * 64) 64
+    let offset = ((i mod 2) lsl (r - 1)) + i / 2 in
+    Cstruct.blit !x 0 b' (offset * 64) 64
   done;
-  o
+  b'
