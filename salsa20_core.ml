c847c691a7d53d0edac372231afa4d6498accf1d
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2016-03-25 00:12:39 +0100

    Extract CStruct blocks of uint32 to an Array.
    Working in the quaterround with the array is both faster and simpler.

--- salsa20_core.ml
+++ salsa20_core.ml
@@ -2,25 +2,18 @@ let salsa_core count i =
   let len = 64 in
   if Cstruct.len i <> len then invalid_arg "input must be 16 blocks of 32 bits"
   else
+    let to_array c = Array.init 16 (fun i -> Cstruct.LE.get_uint32 c (i * 4)) in
     let r a b =
       let rs = 32 - b in
       let open Nocrypto.Numeric.Int32 in (a lsl b) lor (a lsr rs) in
-    let x = Nocrypto.Uncommon.Cs.clone i in
+    let x = to_array i in
     let combine y0 y1 y2 shift =
       let open Nocrypto.Numeric.Int32 in (r (y1 + y2) shift) lxor y0 in
     let quarterround y0 y1 y2 y3 =
-      let a = ref (Cstruct.LE.get_uint32 x (y0 * 4))
-      and b = ref (Cstruct.LE.get_uint32 x (y1 * 4))
-      and c = ref (Cstruct.LE.get_uint32 x (y2 * 4))
-      and d = ref (Cstruct.LE.get_uint32 x (y3 * 4)) in
-      b := combine !b !a !d 7;
-      c := combine !c !b !a 9;
-      d := combine !d !c !b 13;
-      a := combine !a !d !c 18;
-      Cstruct.LE.set_uint32 x (y0 * 4) !a;
-      Cstruct.LE.set_uint32 x (y1 * 4) !b;
-      Cstruct.LE.set_uint32 x (y2 * 4) !c;
-      Cstruct.LE.set_uint32 x (y3 * 4) !d in
+      x.(y1) <- combine x.(y1) x.(y0) x.(y3) 7;
+      x.(y2) <- combine x.(y2) x.(y1) x.(y0) 9;
+      x.(y3) <- combine x.(y3) x.(y2) x.(y1) 13;
+      x.(y0) <- combine x.(y0) x.(y3) x.(y2) 18 in
     for _ = 1 to count do
       quarterround 0 4 8 12;
       quarterround 5 9 13 1;
@@ -34,7 +27,7 @@ let salsa_core count i =
     done;
     let o = Cstruct.create len in
     for j = 0 to 15 do
-      let xj = Cstruct.LE.get_uint32 x (j * 4) in
+      let xj = x.(j) in
       let ij = Cstruct.LE.get_uint32 i (j * 4) in
       let open Nocrypto.Numeric.Int32 in
       Cstruct.LE.set_uint32 o (j * 4) (xj + ij)
