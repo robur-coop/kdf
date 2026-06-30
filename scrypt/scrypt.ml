f68a26d9c50d5809a9ff21b7b4bd668568298dca
Hannes Mehnert <hannes@mehnert.org>
2026-06-30 12:13:06 +0200

    scrypt: tiny optimization

--- scrypt/scrypt.ml
+++ scrypt/scrypt.ml
@@ -1,15 +1,10 @@
 external salsa_core : int -> string -> bytes -> unit = "caml_salsa_core" [@@noalloc]
 
-let salsa20_core count i =
-  let l = 64 in
-  if String.length i <> l then invalid_arg "input must be 16 blocks of 32 bits"
-  else
-    let o = Bytes.create l in
-    salsa_core count i o;
-    Bytes.unsafe_to_string o
-
 let salsa20_8_core i =
-  salsa20_core 4 i
+  let l = 64 in
+  let o = Bytes.create l in
+  salsa_core 4 i o;
+  Bytes.unsafe_to_string o
 
 let scrypt_block_mix b r =
   let b' = Bytes.create (String.length b) in
